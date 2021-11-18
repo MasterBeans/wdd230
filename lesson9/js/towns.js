2a06ec4d4d1ded828bc00ab97d058f4d66934216
MasterBeans <jul21002@byui.edu>
2021-11-18 23:29:18 +0800

    3 towns only

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -10,24 +10,11 @@ fetch(requestURL)
 
     const towns = jsonObject['towns'];
 
-    // "name": "Soda Springs",
-    // "photo": "sodasprings.jpg",
-    // "motto": "Historic Oregon Trail Oasis. The Soda is on Us.",
-    // "yearFounded": 1858,
-    // "currentPopulation": 2985,
-    // "averageRainfall": 15.75,
-    // "events": [
-    //   "February 29: Geyser Song Day",
-    //   "May 1-6: Days of May Celebration",
-    //   "October 15-16: Octoberfest"
-    //]
-
-
     for (let i = 0; i < towns.length; i++) {
       let card = document.createElement('section');
       let cardDetails = document.createElement('div');
 
-      
+
       let h3 = document.createElement('h3');
       let paraMotto = document.createElement('p');
       let paraFounded = document.createElement('p');
@@ -36,13 +23,13 @@ fetch(requestURL)
       let eventsHeader = document.createElement('h4');
       let imgTown = document.createElement('img');
 
-      
+
       h3.textContent = towns[i].name;
       paraMotto.textContent = towns[i].motto;
-      paraFounded.textContent = 'Year Founded: '+towns[i].yearFounded;
-      paraPopulation.textContent = 'Current Population: '+towns[i].currentPopulation;
-      paraRainfall.textContent = 'Average Rainfall: '+towns[i].averageRainfall;
-      paraMotto.setAttribute('id','motto');
+      paraFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+      paraPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+      paraRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+      paraMotto.setAttribute('id', 'motto');
       eventsHeader.textContent = 'Town Events';
 
       cardDetails.appendChild(h3);
@@ -60,9 +47,9 @@ fetch(requestURL)
       }
 
 
-      
 
-     
+
+
 
       imgTown.src = 'images/towns/' + towns[i].photo;
       imgTown.loading = 'lazy';
@@ -73,8 +60,17 @@ fetch(requestURL)
 
       card.appendChild(cardDetails)
       card.appendChild(imgTown);
-     
+
       //card.appendChild(img);
-      document.querySelector('div.cards').appendChild(card);
+      if (towns[i].name == 'Preston') {
+        document.querySelector('div.cards').appendChild(card);
+      }
+      if (towns[i].name == 'Soda Springs') {
+        document.querySelector('div.cards').appendChild(card);
+      }
+      if (towns[i].name == 'Fish Haven') {
+        document.querySelector('div.cards').appendChild(card);
+      }
+
     }
   });
