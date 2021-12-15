638178fe2c478d473226971bc44e3ac57fcc5ead
MesterBeans <byui21002@byui.edu>
2021-12-15 14:14:28 +0800

    home page

---
+++ finalProject/js/weather.js
@@ -0,0 +1,18 @@
+const APPID = "1da91723321396cba3166b2a18fc0112"
+const lat = 11.0384
+const lon = 124.6193
+
+
+const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APPID}&units=imperial`;
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc; 
+    document.getElementById('icon').setAttribute('src', imagesrc); 
+    document.getElementById('icon').setAttribute('alt', desc);
+    
+  });
