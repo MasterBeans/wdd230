5c5d37b21a30a6283f976f6dd2908323dcbd1395
MasterBeans <jul21002@byui.edu>
2021-12-16 08:35:39 +0800

    join and city pages done

---
+++ finalProject/js/javascript.js
@@ -0,0 +1,31 @@
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+let day = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+let month = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+let d = new Date();
+let dayName = day[d.getDay()];
+let monthName = month[d.getMonth()];
+let year = d.getFullYear();
+let currentDate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
--- finalProject/js/weather.js
+++ finalProject/js/weather.js
@@ -3,16 +3,73 @@ const lat = 11.0384
 const lon = 124.6193
 
 
-const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APPID}&units=imperial`;
+const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APPID}&units=metric`;
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
-    const desc = jsObject.weather[0].description;
-    document.getElementById('imagesrc').textContent = imagesrc; 
-    document.getElementById('icon').setAttribute('src', imagesrc); 
-    document.getElementById('icon').setAttribute('alt', desc);
+
+    
+
+    const current = jsObject['current'];
+    document.getElementById('current-weather').textContent = 
+    "Temp : "+ current['temp'] + String.fromCharCode(176) + 'C' + 
+    " | Weather Condition : " + capitalizeFirstLetter(current['weather'][0]['description']) +
+    " | Humidity : " + current['humidity'] + String.fromCharCode(37);
+    
+   
+    const alerts = jsObject['alerts'];
+    if(alerts[0] != null){
+    // document.getElementById('alert').textContent = 
+    // alerts[0]['sender_name'] + " : " + alerts[0]['description'];
+      const div = document.createElement('div');
+      div.setAttribute('class', 'alert');
+
+      const span = document.createElement('span');
+      span.setAttribute('class', 'closebtn');
+      span.setAttribute('onclick', "this.parentElement.style.display='none';");
+      span.innerHTML = '&times;';
+      
+      const para = document.createElement('p');
+      para.setAttribute('id', 'alert')
+      para.textContent = alerts[0]['sender_name'] + " : " + alerts[0]['description'];
+    
+      div.appendChild(span);
+      div.appendChild(para);
+      document.getElementById('alerts').appendChild(div);
+
+      console.log('no alert');
+    }
+    
+
+
+    
+    let date = new Date();
+    const daily = jsObject['daily'];
+   
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday', 'Monday', 'Tuesday', 'Wednesday']
+
+    document.querySelector(`#day1`).textContent = dayOfWeek[date.getDay()+1];
+    document.getElementById('day1-weather').textContent = 
+    "Temp : "+ daily[0]['temp']['max'] + String.fromCharCode(176) + 'C' + 
+    " | Weather Condition : " + capitalizeFirstLetter(daily[0]['weather'][0]['description']) +
+    " | Humidity : " + daily[0]['humidity'] + String.fromCharCode(37);
+    
+    document.querySelector(`#day2`).textContent = dayOfWeek[date.getDay()+2];
+    document.getElementById('day2-weather').textContent = 
+    "Temp : "+ daily[1]['temp']['max'] + String.fromCharCode(176) + 'C' + 
+    " | Weather Condition : " + capitalizeFirstLetter(daily[1]['weather'][0]['description']) +
+    " | Humidity : " + daily[1]['humidity'] + String.fromCharCode(37);
+    
+    document.querySelector(`#day3`).textContent = dayOfWeek[date.getDay()+3];
+    
+    document.getElementById('day3-weather').textContent = 
+    "Temp : "+ daily[2]['temp']['max'] + String.fromCharCode(176) + 'C' + 
+    " | Weather Condition : " + capitalizeFirstLetter(daily[2]['weather'][0]['description']) +
+    " | Humidity : " + daily[2]['humidity'] + String.fromCharCode(37);
     
   });
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
